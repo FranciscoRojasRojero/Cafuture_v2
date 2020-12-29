321e7621ccfcb26d9557dca70870ef353706a534
NightWind <60367109+FranciscoRojasRojero@users.noreply.github.com>
2020-12-28 21:43:32 -0600

    Dulces

--- app/src/main/java/com/francisco/cafuture_v2/SegundoActivity.kt
+++ app/src/main/java/com/francisco/cafuture_v2/SegundoActivity.kt
@@ -2,8 +2,6 @@ package com.francisco.cafuture_v2
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -31,7 +29,7 @@ class SegundoActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.dulces
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
