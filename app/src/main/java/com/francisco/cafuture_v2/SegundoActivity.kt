13a1d403491514368a6a5bb72bfb3483c9c58e59
NightWind <60367109+FranciscoRojasRojero@users.noreply.github.com>
2020-12-29 15:22:51 -0600

    Nuevo main drawer

--- app/src/main/java/com/francisco/cafuture_v2/SegundoActivity.kt
+++ app/src/main/java/com/francisco/cafuture_v2/SegundoActivity.kt
@@ -47,5 +47,4 @@ class SegundoActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 }
